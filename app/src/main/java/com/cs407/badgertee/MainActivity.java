70a6f2a2bfdd5f95c2fea1b7ab1c3cc555847b51
knagpal2 <knagpal2@wisc.edu>
2023-11-07 19:40:26 -0600

    Added Sign On page and navigation

--- app/src/main/java/com/cs407/badgertee/MainActivity.java
+++ app/src/main/java/com/cs407/badgertee/MainActivity.java
@@ -2,9 +2,11 @@ package com.cs407.badgertee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +22,13 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.golf_menu, menu);
         return true;
+    }
+
+    public void navSignOn(View view){
+
+        Intent intent = new Intent(this, sign_up_page.class);
+        startActivity(intent);
+
     }
 
 
---
+++ app/src/main/java/com/cs407/badgertee/sign_up_page.java
@@ -0,0 +1,14 @@
+package com.cs407.badgertee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sign_up_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_page);
+    }
+}
