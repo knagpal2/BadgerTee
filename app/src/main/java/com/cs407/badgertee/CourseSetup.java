7dd8454eb7536892ea92b018efeadee3f11a5040
Cameron Zack <cameronzack@gmail.com>
2023-12-03 22:35:16 -0600

    Adding tees to options, also making it so that options must be selected to progress through game setup

--- app/src/main/java/com/cs407/badgertee/CourseSetup.java
+++ app/src/main/java/com/cs407/badgertee/CourseSetup.java
@@ -5,19 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class CourseSetup extends AppCompatActivity {
+
+    private RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_setup);
+        radioGroup = findViewById(R.id.radioGroup);
     }
 
     public void navGameSetup(View view){
-
-        Intent intent = new Intent(this, GameSetup.class);
-        startActivity(intent);
-
+        if (radioGroup.getCheckedRadioButtonId() == -1) {
+            Toast.makeText(this, "Please select a Tee", Toast.LENGTH_SHORT).show();
+        } else {
+            Intent intent = new Intent(this, GameSetup.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/cs407/badgertee/GameSetup.java
+++ app/src/main/java/com/cs407/badgertee/GameSetup.java
@@ -5,19 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class GameSetup extends AppCompatActivity {
 
+    private RadioGroup radioGroup;
+    private RadioGroup radioGroup3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_setup);
+        radioGroup = findViewById(R.id.radioGroup);
+        radioGroup3 = findViewById(R.id.radioGroup3);
     }
 
     public void navRangeFinder(View view){
 
-        Intent intent = new Intent(this, RangeFinder.class);
-        startActivity(intent);
-
+        if (radioGroup.getCheckedRadioButtonId() == -1 || radioGroup3.getCheckedRadioButtonId() == -1) {
+            Toast.makeText(this, "Please select options in both groups", Toast.LENGTH_SHORT).show();
+        } else {
+            Intent intent = new Intent(this, RangeFinder.class);
+            startActivity(intent);
+        }
     }
 }
